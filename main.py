17742d381dffdb86e7b73c374f998ce3545b1044
Elliot <EKnock211@gmail.com>
2022-06-02 13:53:46 +0100

    Detects when player has reached the other side.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
+screen.title("Turtle Crossing")
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
@@ -27,5 +28,10 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+
+    # Detects successful crossing.
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
 
 screen.exitonclick()
